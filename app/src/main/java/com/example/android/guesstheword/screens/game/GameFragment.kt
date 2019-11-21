7ce5a0b2b44ae677b889b2918e5b3d88ad71c75c
Administrator <sardar@mypaperlessoffice.com>
2019-11-22 02:31:10 +0500

    add timer to the app.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -64,6 +65,12 @@ class GameFragment : Fragment() {
 
         viewModel.word.observe(this, Observer {newWord->
             binding.wordText.text = newWord.toString()
+        })
+
+        // You can use DateUtils.formatElapsedTime to correctly format the long to a time string
+        viewModel.currentTime.observe(this, Observer { newTime ->
+            binding.timerText.text = DateUtils.formatElapsedTime(newTime)
+
         })
 
         viewModel.gameFinished.observe(this, Observer {isGameFinished->
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,29 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel :ViewModel(){
+
+
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
+
+    private val timer: CountDownTimer
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     // The current word
      private val _word = MutableLiveData<String>()
@@ -31,6 +49,18 @@ class GameViewModel :ViewModel(){
         _score.value = 0
         _word.value = ""
         _gameFinished.value = false
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _gameFinished.value = true
+            }
+        }
+        timer.start()
     }
 
     /**
@@ -69,10 +99,10 @@ class GameViewModel :ViewModel(){
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            _gameFinished.value = true
-        } else {
+            resetList()
             _word.value = wordList.removeAt(0)
         }
+        _word.value = wordList.removeAt(0)
     }
 
     /** Methods for buttons presses **/
@@ -94,6 +124,7 @@ class GameViewModel :ViewModel(){
     override fun onCleared() {
         super.onCleared()
         Log.d(this@GameViewModel.javaClass.simpleName, "onCleared: ");
+        timer.cancel()
     }
 
 
